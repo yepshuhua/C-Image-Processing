7db582e38804ded2d4175922ccc4c863f72598fd
吴南骏 <1062173632@qq.com>
2020-09-17 11:10:43 +0800

    C#添加修改图片处理

---
+++ ImgGrayTest/ImgGrayTest/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ImgGrayTest
+{
+    partial class ImgGrayTest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.Pixel = new System.Windows.Forms.Button();
+            this.Menory = new System.Windows.Forms.Button();
+            this.pointer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(37, 46);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "打开图片";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(37, 92);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "保存图片";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(37, 138);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 2;
+            this.Close.Text = "关闭";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Pixel
+            // 
+            this.Pixel.Location = new System.Drawing.Point(37, 200);
+            this.Pixel.Name = "Pixel";
+            this.Pixel.Size = new System.Drawing.Size(75, 23);
+            this.Pixel.TabIndex = 3;
+            this.Pixel.Text = "提取像素法";
+            this.Pixel.UseVisualStyleBackColor = true;
+            this.Pixel.Click += new System.EventHandler(this.Pixel_Click);
+            // 
+            // Menory
+            // 
+            this.Menory.Location = new System.Drawing.Point(37, 246);
+            this.Menory.Name = "Menory";
+            this.Menory.Size = new System.Drawing.Size(75, 23);
+            this.Menory.TabIndex = 4;
+            this.Menory.Text = "内存法";
+            this.Menory.UseVisualStyleBackColor = true;
+            this.Menory.Click += new System.EventHandler(this.Menory_Click);
+            // 
+            // pointer
+            // 
+            this.pointer.Location = new System.Drawing.Point(37, 292);
+            this.pointer.Name = "pointer";
+            this.pointer.Size = new System.Drawing.Size(75, 23);
+            this.pointer.TabIndex = 5;
+            this.pointer.Text = "指针法";
+            this.pointer.UseVisualStyleBackColor = true;
+            // 
+            // ImgGrayTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.pointer);
+            this.Controls.Add(this.Menory);
+            this.Controls.Add(this.Pixel);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Name = "ImgGrayTest";
+            this.Text = "ImgGrayTest";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ImgGrayTest_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Pixel;
+        private System.Windows.Forms.Button Menory;
+        private System.Windows.Forms.Button pointer;
+    }
+}
+
---
+++ ImgGrayTest/ImgGrayTest/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgGrayTest
+{
+    public partial class ImgGrayTest : Form
+    {
+        public ImgGrayTest()
+        {
+            InitializeComponent();
+        }
+        private string curFileName;
+        private System.Drawing.Bitmap curBitmap;
+        //打开图像文件
+        private void Open_Click(object sender, EventArgs e)
+        {
+            //创建openFileDialog
+            OpenFileDialog opnlg = new OpenFileDialog();
+            //为图像选择一个筛选器
+            opnlg.Filter = "所有图像｜*.bmp;*.pcx;*.png;*,jpg;*.gif;" + "*.tif;*.ico;*.dxf;*.cgm;*.cdr;*.wmf'*.eps;.emf|" +
+                "位图(*.bmp;*.jpg;*.png;...)|*.bmp;*.pcx;*.png;*.jpg;*.gif;*.tif;*.ico|" +
+                "矢量图（*.wmf;*.eps;*.emf;...)|*.dxf;*.cgm;*.cdr;*.wmf;*.eps;*.emf|";
+            //设置对话框标题
+            opnlg.Title = "打开图像文件";
+            //启动”帮助“按钮
+            opnlg.ShowHelp = true;
+            //如果结果为打开，选定文件
+            if (opnlg.ShowDialog() == DialogResult.OK)
+            {
+                curFileName = opnlg.FileName;    //读取当前选中的文件名
+                //使用Image.fromFile创建图像对象
+                try
+                {
+                    curBitmap = (Bitmap)Image.FromFile(curFileName);
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message); //抛出异常
+                }
+            }
+            Invalidate(); //对窗体进行重新绘制，这将强行执行paint事件处理程序 
+        }
+
+        private void ImgGrayTest_Paint(object sender, PaintEventArgs e)
+        {
+            //获取Graphics对象
+            Graphics g = e.Graphics;
+            if (curBitmap != null)
+            {
+                //使用DrawImage方法绘制图像
+                //160，20：显示在主窗体内，图像左上角的坐标
+                //curBitmap.Width,curBitmap.Height:图像的宽度和高度
+                g.DrawImage(curBitmap, 160, 20, curBitmap.Width, curBitmap.Height);
+            }
+
+        }
+        //保存图像文件
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (curBitmap == null)
+                return;
+            SaveFileDialog savelog = new SaveFileDialog(); //调用saveFileDialog
+            savelog.Title = "保存为"; //设置对话标题
+            savelog.OverwritePrompt = true; //改写已存在文件时提示用户
+            savelog.Filter = "BMP文件(*.bmp)|*.bmp|" + "Gif文件（*.gif)|*.gif|" + "JPEG文件(*.jpg)|*.jpg|" + "PNG文件(*.pgn)|*.png"; //为图像选择一个筛选器
+            savelog.ShowHelp = true; //启动帮助按钮
+                                     //如果选择了格式，则保存图像
+            if (savelog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savelog.FileName; //获取用户选择文件
+                string strFileExtn = fileName.Remove(0, fileName.Length - 3); //获取用户选择文件的扩展名
+                switch (strFileExtn) //保存文件
+                {
+                    case "bmp":  //bmp格式
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "jpg": //jpg格式
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg); break;
+                    case "gif": //gif格式
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif); break;
+                    case "tif": //tif格式
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "png":
+                        //png格式
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png); break; ;
+                    default: break;
+                }
+            }
+
+        }
+        //关闭窗口
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Pixel_Click(object sender, EventArgs e)
+        {
+            if (curBitmap != null)
+            {
+                Color curColor;
+                int ret;
+                for(int i = 0; i < curBitmap.Width; i++)//二维数组循环
+                {
+                    for(int j = 0; j < curBitmap.Height; j++)
+                    {
+                        curColor = curBitmap.GetPixel(i, j);//获取该点像素的RGB值
+                        ret = (int)(curColor.R * 0.2229 + curColor.G * 0.587 + curColor.B * 0.114);//计算灰度值
+                        curBitmap.SetPixel(i, j, Color.FromArgb(ret, ret, ret));//设置像素点的灰度值R=G=B=ret
+                    }
+                }
+            }
+            Invalidate(); //对窗体进行重新绘制，这将强行执行paint事件处理程序 
+        }
+
+        private void Menory_Click(object sender, EventArgs e)
+        {
+            if (curBitmap != null)
+            {
+                //位图矩形
+                Rectangle rect = new Rectangle(0, 0, curBitmap.Width, curBitmap.Height);
+                //以可读写方式锁定全部位图像素
+                System.Drawing.Imaging.BitmapData bmpData = curBitmap.LockBits
+                    (rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, curBitmap.PixelFormat);
+                //获得首地址
+                IntPtr ptr = bmpData.Scan0;
+                //24位bmp位图字节数
+                int bytes = curBitmap.Width * curBitmap.Height * 3;
+                //定义位图数组
+                byte[] rgbValues = new byte[bytes];
+                //复制被锁定的位图像素值到该数组内
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                //灰度化
+                double colorTemp = 0;
+            }
+        }
+    }
+}
